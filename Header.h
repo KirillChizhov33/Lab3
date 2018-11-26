f10b1d18e62828346606aea38d466596d8de2c1c
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2018-11-26 22:06:47 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,1 +1,145 @@
-header.h
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+class Stack
+{
+	int Size;
+	int Index;
+	int * Mem;
+	Stack() {};
+public:
+	Stack(int _Size);
+	Stack(const Stack &b);
+	Stack & operator =(const Stack &b);
+	bool IsStackEmpty();
+	bool IsStackFull();
+	int GetSize();
+	void AddElement(int Elem);
+	int DeleteElement();
+	void DeleteAllElements();
+	friend ostream & operator<<(ostream &os, const Stack &v);
+};
+
+class Turn
+{
+	int * Mem;
+	int Size;
+	int LowIndex;
+	int HighIndex;
+public:
+	Turn() {};
+	Turn(int _Size);
+	Turn(const Turn &b);
+	bool IsTurnEmpty();
+	bool IsTurnFull();
+	void AddElement(int Elem);
+	int DeleteElement();
+	int & operator [](int index);
+	friend ostream & operator<<(ostream &os, const Turn &v);
+};
+
+class TValue
+{
+public:
+	virtual int prior() = 0;
+};
+
+class Top : public TValue
+{
+public:
+	Top(char c) {};
+	int prior() 
+	{ 
+		return 0;
+	}
+};
+
+class Tint : public TValue
+{
+public:
+	Tint(int v) {};
+	int prior()
+	{
+		return 0;
+	}
+};
+
+class TQ : public Turn
+{
+public: 
+	void push(TValue *v){}
+
+};
+
+class TLex
+{
+	TQ queue;
+	std::string s;
+public:
+	TLex(TQ _queue)
+	{
+		queue = _queue;
+	}
+	int pos(char *s, char c)
+	{
+		int i = 0;
+		while (s[i] != '\0')
+		{
+			if (s[i] == c)
+			{
+				return i;
+				i++;
+			}
+		}
+		return -1;
+	}
+	TQ & convert(char * str)
+	{
+		int st = 0;
+		int i = 0;
+		char c;
+		char op[] = "+-*/()";
+		while (str[i] != '\0')
+		{
+			c = str[i];
+			i++;
+			if (st == 0)
+			{
+				if (c == ' ') {}
+				if (pos(op, c) >= 0)
+				{
+					queue.push(new Top(c));
+				}
+			}
+			if ((c >= '0') && (c <= '9'))
+			{
+				s = c;
+				st = 1;
+			}
+			if (st == 1)
+			{
+				if ((c >= '0') && (c <= '9'))
+				{
+					s += c;
+				}
+				if (c == ' ')
+				{
+					queue.push(new Tint(atoi(s.c_str())));
+					st = 0;
+				}
+				if (pos(op, c) >= 0)
+				{
+					queue.push(new Tint(atoi(s.c_str())));
+					queue.push(new Top(c));
+					st = 0;
+				}
+			}
+		}
+		if (st == 1)
+		{
+			queue.push(new Tint(atoi(s.c_str())));
+			return queue;
+		}
+	}
+};

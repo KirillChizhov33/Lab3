c3f7d1a11b5d00c2d99db03de892d4f1d17b8285
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2018-11-29 19:59:13 +0300

    Version 0.0.8

--- Sourse.cpp
+++ Sourse.cpp
@@ -10,6 +10,7 @@ Stack::Stack(const Stack &b)
 {
 	Size = b.Size;
 	Index = b.Index;
+	Mem = new int[Size];
 	for (int i = 0; i < Index; i++)
 	{
 		Mem[i] = b.Mem[i];
@@ -38,7 +39,7 @@ bool Stack::IsStackFull()
 }
 int Stack::GetSize()
 {
-	return Index;
+	return Index+1;
 }
 void Stack::AddElement(int Elem)
 {
@@ -106,6 +107,33 @@ bool Turn::IsTurnFull()
 {
 	return (LowIndex == (HighIndex + 2) % TSize);
 }
+/*bool Turn::IsStringCorrect()
+{
+	int i = LowIndex;
+	int cnt = 0;
+	TValue * temp1 = new Top(')');
+	TValue * temp2 = new Top('(');
+	TValue * temp3 = new Top('+');
+	TValue * temp4 = new Top('-');
+	TValue * temp5 = new Top('*');
+	TValue * temp6 = new Top('/');
+	while (i != (HighIndex + 1) % TSize)
+	{
+		if (TMem[i] == temp1)
+		{
+			cnt--;
+			if (cnt < 0) { return false; }
+		}
+		if (TMem[i] == temp2)
+		{
+			cnt++;
+		}
+		if ((TMem[i] != temp3) && (TMem[i] != temp4) && (TMem[i] != temp5) && (TMem[i] != temp6) && sizeof(TMem[i]) == 4)
+		{
+
+		}
+	}
+}*/
 void Turn::TAddElement(TValue * Elem)
 {
 	if (IsTurnFull())
@@ -138,9 +166,74 @@ ostream & operator <<(ostream &os, TValue &v)
 
 ostream & operator<<(ostream &os, const Turn &v)
 {
-	for (int i = 0; i < v.HighIndex + 1; i++)
+	for (int i = 0; i < (v.HighIndex + 1) % v.TSize; i++)
 	{
 		cout << *(v.TMem[i]);
 	}
 	return os;
+}
+int POS(char *s, char c)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return i;
+
+		}
+		i++;
+	}
+	return -1;
+}
+bool StrCor(char *string)
+{
+	int i = 0;
+	int cnt = 0;
+	char s[] = "+-*/()";
+	char t[] = "+-*/";
+	if (i == 0)
+	{
+		if (POS(t, string[i]) >= 0)
+		{
+			return false;
+		}
+	}
+	while (string[i] != '\0')
+	{
+		if (string[i] == ')')
+		{
+			cnt--;
+			if (cnt < 0)
+			{
+				return false;
+			}
+		}
+		if (i > 0)
+		{
+			if ((POS(s, string[i]) >= 0) && (POS(s, string[i - 1]) >= 0))
+			{
+				return false;
+			}
+		}
+		if (string[i] == '(')
+		{
+			cnt++;
+		}
+		if ((((string[i] >= '0') && (string[i] <= '9')) || (POS(s, string[i]) >= 0))==false)
+		{
+			return false;
+		}
+		i++;
+	}
+	i--;
+	if (POS(t, string[i]) >= 0)
+	{
+		return false;
+	}
+	if (cnt > 0)
+	{
+		return false;
+	}
+	return true;
 }
